03f5056afbc971ba750be0caf79680f51335cf51
Giorgi.Kirvalidze@kandasoft.com <102462724+Zkirvalidze@users.noreply.github.com>
2022-09-12 15:57:47 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>this is the home</div>;
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from './Footer';
+import Hero from './Hero';
+import Layout from './Layout';
+import Navbar from './Navbar';
+import ProductDetails from './ProductDetails';
+import ProductList from './ProductList';
+
+export { Navbar, Footer, Hero, Layout, ProductDetails, ProductList };
