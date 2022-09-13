daf6939b1763235ce1cccbc2ecb9003f5bba8dbc
Giorgi.Kirvalidze@kandasoft.com <102462724+Zkirvalidze@users.noreply.github.com>
2022-09-13 20:40:44 +0400

    product list added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+import { Link } from 'react-router-dom';
+import { urlFor } from '../lib/client';
+const ProductDetails = ({
+  product: { name, image, price, description, slug },
+}) => {
+  return (
+    <div className="">
+      <Link to={`/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])
+              .width(250)
+              .height(250)}
+            className="product-image"
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ProductDetails;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { client } from '../lib/client';
-
+import { Link } from 'react-router-dom';
+import { ProductDetails } from './index';
 const ProductList = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -13,8 +14,15 @@ const ProductList = () => {
       .then((data) => setData(data))
       .catch((err) => console.log(err));
   }, []);
-  console.log(data);
-  return <div>ProductList</div>;
+
+  return (
+    <div className=" grid grid-cols-3 place-content-center  justify-items-center mt-28 gap-4 w-full h-full">
+      {data &&
+        data.map((product, index) => (
+          <ProductDetails key={index} product={product} />
+        ))}
+    </div>
+  );
 };
 
 export default ProductList;
